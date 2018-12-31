dc0ee4bd53adfd2335510607f1de9920192d3a1e
Josh Goldberg <josh@fullscreenmario.com>
2018-12-31 10:43:07 -0500

    Re-added buggy test files back

---
+++ test/cases/unit/property accesses/missingProperties/expected.ts
@@ -0,0 +1,48 @@
+{
+    function _(
+        useForString: string,
+        useForStringOrNull: string | null,
+        useForStringOrUndefined: string | undefined,
+        useForStringOrNullOrUndefined: string | null | undefined,
+    ) {
+        useForString.length;
+        useForStringOrNull.length;
+        useForStringOrUndefined.length;
+        useForStringOrNullOrUndefined.length;
+
+        let stringExplicit: string = "";
+        stringExplicit.length;
+
+        let stringImplicit = "";
+        stringImplicit.length;
+
+        let stringOrUndefinedExplicit: string | undefined = undefined;
+        stringOrUndefinedExplicit.length;
+
+        let stringOrUndefinedImplicit = "" as string | undefined;
+        stringOrUndefinedImplicit.length;
+
+        let stringOrNullExplicit: string | null = "" as string | null;
+        stringOrNullExplicit.length;
+
+        let stringOrNullImplicit = "" as null | undefined;
+        stringOrNullImplicit.length;
+
+        let stringOrNullOrUndefinedExplicit: string | null | undefined = null;
+        stringOrNullOrUndefinedExplicit.length;
+
+        let stringOrNullOrUndefinedImplicit = "" as string | null | undefined;
+        stringOrNullOrUndefinedImplicit.length;
+    }
+
+    class Abc {
+givenNumber: number;
+givenNumberOrUndefined: number;
+givenUndefined: any;
+        def() {
+            this.givenNumber = 1;
+            this.givenNumberOrUndefined = 1 as number | undefined;
+            this.givenUndefined = undefined;
+        }
+    }
+}
