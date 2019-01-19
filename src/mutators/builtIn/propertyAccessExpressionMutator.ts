0a16da835e85ace1bc2edb3607f9bd87e118ec00
Josh Goldberg <josh@fullscreenmario.com>
2019-01-19 12:21:34 -0500

    Stopped allowing multiple properties of the same name added to classes
    Fixes 112

--- src/mutators/builtIn/propertyAccessExpressionMutator.ts
+++ src/mutators/builtIn/propertyAccessExpressionMutator.ts
@@ -12,21 +12,32 @@ export const propertyAccessExpressionMutator: FileMutator = (request: FileMutati
         return [];
     }
 
+    // If an undeclared property is referenced multiple times, TypeScript will suggest adding it in each time
+    // We therefore only suggest each property name once per wave
+    // In theory, we could also respect each node name per class, but that's hard, and it's rare to have many classes per file
+    const suggestedMissingProperties = new Set<string>();
+
+    const visitPropertyAccessExpression = (node: ts.PropertyAccessExpression, request: FileMutationsRequest): IMutation | undefined => {
+        // If the access should create a missing property, go for that
+        const missingPropertyFix = getMissingPropertyMutations(request, node);
+        if (missingPropertyFix !== undefined) {
+            // Don't suggest this missing property if a node of this name was already added
+            if (suggestedMissingProperties.has(node.name.text)) {
+                return undefined;
+            }
+
+            suggestedMissingProperties.add(node.name.text);
+            return missingPropertyFix;
+        }
+
+        // If we can fix a strict null check fix here, try that
+        const strictPropertyAccessFix = getStrictPropertyAccessFix(request, node);
+        if (strictPropertyAccessFix !== undefined) {
+            return strictPropertyAccessFix;
+        }
+
+        return undefined;
+    };
+
     return collectMutationsFromNodes(request, ts.isPropertyAccessExpression, visitPropertyAccessExpression);
-};
-
-const visitPropertyAccessExpression = (node: ts.PropertyAccessExpression, request: FileMutationsRequest): IMutation | undefined => {
-    // If the access should create a missing property, go for that
-    const missingPropertyFix = getMissingPropertyMutations(request, node);
-    if (missingPropertyFix !== undefined) {
-        return missingPropertyFix;
-    }
-
-    // If we can fix a strict null check fix here, try that
-    const strictPropertyAccessFix = getStrictPropertyAccessFix(request, node);
-    if (strictPropertyAccessFix !== undefined) {
-        return strictPropertyAccessFix;
-    }
-
-    return undefined;
 };
--- test/cases/unit/property accesses/typeAliases/expected.ts
+++ test/cases/unit/property accesses/typeAliases/expected.ts
@@ -40,6 +40,12 @@
             this.givenNumber = 1;
             this.givenNumberOrUndefined = 1 as number | undefined;
             this.givenUndefined = undefined;
+
+            this.givenTwiceSame = 1;
+            this.givenTwiceSame = 1;
+
+            this.givenTwiceDifferent = 1;
+            this.givenTwiceDifferent = undefined;
         }
     }
 }
