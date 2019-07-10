1f400021528aee3d52aceefdfcd941cf63dd6583
joshuakgoldberg <joshuakgoldberg@outlook.com>
2019-07-10 14:31:20 -0700

    Fixed miscellaneous bugs around React component signatures
    When the props node doesn't have any declarations, getComponentPropsNode shouldn't bother trying to look through them.
    
    I haven't filed bugs about this bug I remember there being crashes associated...

--- src/mutators/builtIn/fixIncompleteTypes/fixIncompleteReactTypes/reactFiltering/isReactComponentNode.ts
+++ src/mutators/builtIn/fixIncompleteTypes/fixIncompleteReactTypes/reactFiltering/isReactComponentNode.ts
@@ -8,18 +8,23 @@ export type ReactClassComponentNode = ts.ClassDeclaration | ts.ClassExpression;
 
 export type ReactFunctionalComponentNode = ts.ArrowFunction | ts.FunctionDeclaration | ts.FunctionExpression;
 
+/**
+ * @returns Whether the node is able to be a React component node.
+ */
 export const isReactComponentNode = (node: ts.Node): node is ReactComponentNode => {
+    // Functions can generally be React components if they have 0 or 1 parameters
     if (ts.isArrowFunction(node) || ts.isFunctionDeclaration(node) || ts.isFunctionExpression(node)) {
-        return true;
+        return node.parameters.length <= 1;
     }
 
+    // Otherwise, we only look at class declarations
     if (!ts.isClassDeclaration(node)) {
         return false;
     }
 
     const extendsType = getClassExtendsType(node);
 
-    return extendsType === undefined ? false : extensionExpressionIsReactComponent(extendsType);
+    return extendsType !== undefined && extensionExpressionIsReactComponent(extendsType);
 };
 
 const extensionExpressionIsReactComponent = (node: ts.ExpressionWithTypeArguments): boolean => {
