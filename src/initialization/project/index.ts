f7adf01586ca3f159e161a4aed2e4b3baa478675
Josh Goldberg <josh@fullscreenmario.com>
2019-07-17 16:24:38 -0700

    Added project/index.ts

---
+++ src/initialization/project/index.ts
@@ -0,0 +1,38 @@
+import { prompt } from "enquirer";
+
+const custom = "custom";
+
+const choices = ["./tsconfig.json", "./src/tsconfig.json", custom];
+
+export const initializeProject = async () => {
+    const project = await initializeBuiltInProject();
+
+    return project === custom ? initializeCustomProject() : project;
+};
+
+const initializeBuiltInProject = async () => {
+    const { project } = await prompt([
+        {
+            choices,
+            initial: choices[choices.length - 1],
+            message: "Where is your tsconfig.json?",
+            name: "project",
+            type: "select",
+        },
+    ]);
+
+    return project as string;
+};
+
+const initializeCustomProject = async () => {
+    const { project } = await prompt([
+        {
+            initial: "./tsconfig.json",
+            message: "Where is your tsconfig.json?",
+            name: "project",
+            type: "text",
+        },
+    ]);
+
+    return project as string;
+};
