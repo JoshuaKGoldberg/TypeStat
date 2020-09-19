b34a9c6bb0bff515368dc26ab163b869ea16e2b4
Josh Goldberg <me@joshuakgoldberg.com>
2020-09-19 01:20:34 -0400

    More disables

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,12 +20,15 @@ module.exports = {
         "@typescript-eslint/prefer-regexp-exec": "off",
 
         // These rules are off because we don't want them
+        "@typescript-eslint/ban-types": "off",
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/explicit-module-boundary-types": "off",
         "@typescript-eslint/no-empty-function": "off",
         "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
         "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-return": "off",
         "@typescript-eslint/no-use-before-define": "off",
         "@typescript-eslint/no-var-requires": "off",
         "@typescript-eslint/require-await": "off",
