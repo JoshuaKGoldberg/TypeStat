39db8c412e9a81802d02f1c736c7dffd083c9bf0
Josh Goldberg <me@joshuakgoldberg.com>
2020-11-29 17:21:50 -0500

    Oops, deleted runTests.ts...

---
+++ src/tests/runTests.ts
@@ -0,0 +1,76 @@
+import { describeMutationTestCases } from "automutate-tests";
+import { Command } from "commander";
+import * as fs from "fs";
+import * as path from "path";
+
+import { requireExposedTypeScript } from "../mutations/createExposedTypeScript";
+import { fillOutRawOptions } from "../options/fillOutRawOptions";
+import { RawTypeStatOptions, TypeStatOptions } from "../options/types";
+import { createTypeStatProvider } from "../runtime/createTypeStatProvider";
+import { arrayify } from "../shared/arrays";
+
+interface ParsedTestArgv {
+    accept?: boolean;
+    include?: string;
+}
+
+const parsed = new Command()
+    // Allow unknown options for the case of IDE debuggers who directly write to process.argv
+    // If this line is removed, VS Code debugging will break 😲
+    .allowUnknownOption(true)
+    .option("-i, --include [include]", "path to a TypeScript project file")
+    .option("-a, --accept", "override existing expected results instead of asserting")
+    .parse(process.argv) as ParsedTestArgv;
+
+// Modify TypeScript here so that no tests incur the performance penalty of doing it themselves
+const ts = requireExposedTypeScript();
+
+const rawPathToRegExp = (rawPath: string) => new RegExp(`.*${rawPath.split(/\\|\//g).slice(-3).join(".*")}.*`, "i");
+
+// The .vscode/launch.json task adds includes via this environment variable
+const includes = [...arrayify(parsed.include ?? []).map(rawPathToRegExp), ...arrayify(process.env.TEST_GLOB).map(rawPathToRegExp)];
+
+describeMutationTestCases(
+    path.join(__dirname, "../../test"),
+    (fileName: string, typeStatPath: string | undefined) => {
+        if (typeStatPath === undefined) {
+            throw new Error(`Could not find typestat.json for ${fileName}.`);
+        }
+
+        const projectDirectory = path.dirname(typeStatPath);
+        const rawOptions = JSON.parse(fs.readFileSync(typeStatPath).toString()) as RawTypeStatOptions;
+
+        const projectPath = path.join(projectDirectory, "tsconfig.json");
+        const rawCompilerOptions = fs.readFileSync(typeStatPath).toString();
+        const compilerOptions = ts.parseConfigFileTextToJson(typeStatPath, rawCompilerOptions).config;
+        const output = {
+            log: jest.fn(),
+            stderr: jest.fn(),
+            stdout: jest.fn(),
+        };
+
+        return createTypeStatProvider({
+            ...(fillOutRawOptions({
+                argv: { args: [] },
+                compilerOptions,
+                cwd: path.dirname(projectPath),
+                output,
+                projectPath,
+                rawOptions: {
+                    ...rawOptions,
+                    projectPath,
+                },
+            }) as TypeStatOptions),
+            fileNames: [fileName],
+        });
+    },
+    {
+        accept: parsed.accept || !!process.env.TEST_ACCEPT,
+        actual: (original) => (original.endsWith("x") ? "actual.tsx" : "actual.ts"),
+        expected: (original) => (original.endsWith("x") ? "expected.tsx" : "expected.ts"),
+        includes,
+        normalizeEndlines: "\n",
+        original: "./original.*",
+        settings: "typestat.json",
+    },
+);
