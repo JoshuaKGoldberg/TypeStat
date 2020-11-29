f07b0a1477a33e55d8643366f89f4c7c94b49145
Josh Goldberg <me@joshuakgoldberg.com>
2020-11-29 14:55:31 -0500

    WIP: trying to get proper state resolutions...

--- test/cases/fixes/incompleteTypes/implicitGenerics/incompleteImplicitClassGenerics/expected.ts
+++ test/cases/fixes/incompleteTypes/implicitGenerics/incompleteImplicitClassGenerics/expected.ts
@@ -1,50 +1,99 @@
+import React, { Component } from 'react';
+
 (function () {
-    class BaseWithoutGenerics { }
-    class BaseWithOneGeneric<T> { constructor(t: T) { } }
-    class BaseWithTwoGenerics<T, U> {constructor(t: T, u: U) {} }
+    // Straightforward generics
 
-    class ExtendsBaseWithout extends BaseWithoutGenerics { }
-    new ExtendsBaseWithout();
+    // class BaseWithoutGenerics { }
+    // class BaseWithOneGeneric<T> { constructor(t: T) { } }
+    // class BaseWithTwoGenerics<T, U> {constructor(t: T, u: U) {} }
 
+    // class ExtendsBaseWithout extends BaseWithoutGenerics { }
+    // new ExtendsBaseWithout();
 
+    // class ExtendsBaseWithOneLiteral extends BaseWithOneGeneric {
+    //     constructor() {
+    //         super('abc')
+    //     }
+    // }
 
-    class ExtendsBaseWithOneLiteral extends BaseWithOneGeneric<string> {
-        constructor() {
-            super('abc')
-        }
-    }
+    // interface OneInterface {
+    //     property: string;
+    // }
+    // const oneInterface: OneInterface = { property: 'abc' };
 
-    interface OneInterface {
-        property: string;
-    }
-    const oneInterface: OneInterface = { property: 'abc' };
+    // class ExtendsBaseWithOneInterface extends BaseWithOneGeneric {
+    //     constructor() {
+    //         super(oneInterface)
+    //     }
+    // }
 
+    // type OneType = {
+    //     property: string[];
+    // }
+    // const oneType: OneType = { property: ['a', 'b', 'c'] };
 
+    // class ExtendsBaseWithOneType extends BaseWithOneGeneric {
+    //     constructor() {
+    //         super(oneType)
+    //     }
+    // }
 
-    class ExtendsBaseWithOneInterface extends BaseWithOneGeneric<OneInterface> {
-        constructor() {
-            super(oneInterface)
-        }
-    }
+    // class ExtendsBaseWithTwo extends BaseWithTwoGenerics {
+    //     constructor() {
+    //         super(123, false)
+    //     }
+    // }
 
-    type OneType = {
-        property: string[];
-    }
-    const oneType: OneType = { property: ['a', 'b', 'c'] };
+    // Member object (e.g. for React state)
 
+    // class MemberImmediateBase<First = {}, Second = {}> {
+    //     member: Second;
 
+    //     setMember(member: Second) {
+    //         return member;
+    //     }
+    // }
 
-    class ExtendsBaseWithOneType extends BaseWithOneGeneric<OneType> {
-        constructor() {
-            super(oneType)
-        }
-    }
+    // class MemberImmediate extends MemberImmediateBase {
+    //     member = {
+    //         key: false,
+    //     };
 
+    //     addToState = () => {
+    //         this.setMember({ key: true });
+    //     };
+    // }
 
+    // class MemberCurriedBase<First = {}, Second = {}> {
+    //     member: Second;
 
-    class ExtendsBaseWithTwo extends BaseWithTwoGenerics<number, boolean> {
-        constructor() {
-            super(123, false)
-        }
+    //     setMember(getMember: (oldMember: Second) => Second) {
+    //         getMember(this.member);
+    //     }
+    // }
+
+    // class MemberCurried extends MemberCurriedBase {
+    //     member = {
+    //         key: false,
+    //     };
+
+    //     addToState = () => {
+    //         this.setMember(previousMember => ({
+    //             key: !previousMember.key,
+    //         }));
+    //     };
+    // }
+
+    // Temp: trying to create minimum repro from client TitleEdit...
+
+    class TitleEdit extends Component {
+        state = {
+            hasDeleteRevealed: false,
+        };
+
+        toggleDeleteRevealed = () =>
+            this.setState(prevState => ({
+                hasDeleteRevealed: !prevState.hasDeleteRevealed,
+            }));
     }
 })();
--- test/cases/fixes/incompleteTypes/implicitGenerics/incompleteImplicitClassGenerics/original.ts
+++ test/cases/fixes/incompleteTypes/implicitGenerics/incompleteImplicitClassGenerics/original.ts
@@ -1,42 +1,99 @@
+import React, { Component } from 'react';
+
 (function () {
-    class BaseWithoutGenerics { }
-    class BaseWithOneGeneric<T> { constructor(t: T) { } }
-    class BaseWithTwoGenerics<T, U> {constructor(t: T, u: U) {} }
+    // Straightforward generics
 
-    class ExtendsBaseWithout extends BaseWithoutGenerics { }
-    new ExtendsBaseWithout();
+    // class BaseWithoutGenerics { }
+    // class BaseWithOneGeneric<T> { constructor(t: T) { } }
+    // class BaseWithTwoGenerics<T, U> {constructor(t: T, u: U) {} }
 
-    class ExtendsBaseWithOneLiteral extends BaseWithOneGeneric {
-        constructor() {
-            super('abc')
-        }
-    }
+    // class ExtendsBaseWithout extends BaseWithoutGenerics { }
+    // new ExtendsBaseWithout();
 
-    interface OneInterface {
-        property: string;
-    }
-    const oneInterface: OneInterface = { property: 'abc' };
+    // class ExtendsBaseWithOneLiteral extends BaseWithOneGeneric {
+    //     constructor() {
+    //         super('abc')
+    //     }
+    // }
 
-    class ExtendsBaseWithOneInterface extends BaseWithOneGeneric {
-        constructor() {
-            super(oneInterface)
-        }
-    }
+    // interface OneInterface {
+    //     property: string;
+    // }
+    // const oneInterface: OneInterface = { property: 'abc' };
 
-    type OneType = {
-        property: string[];
-    }
-    const oneType: OneType = { property: ['a', 'b', 'c'] };
+    // class ExtendsBaseWithOneInterface extends BaseWithOneGeneric {
+    //     constructor() {
+    //         super(oneInterface)
+    //     }
+    // }
 
-    class ExtendsBaseWithOneType extends BaseWithOneGeneric {
-        constructor() {
-            super(oneType)
-        }
-    }
+    // type OneType = {
+    //     property: string[];
+    // }
+    // const oneType: OneType = { property: ['a', 'b', 'c'] };
 
-    class ExtendsBaseWithTwo extends BaseWithTwoGenerics {
-        constructor() {
-            super(123, false)
-        }
+    // class ExtendsBaseWithOneType extends BaseWithOneGeneric {
+    //     constructor() {
+    //         super(oneType)
+    //     }
+    // }
+
+    // class ExtendsBaseWithTwo extends BaseWithTwoGenerics {
+    //     constructor() {
+    //         super(123, false)
+    //     }
+    // }
+
+    // Member object (e.g. for React state)
+
+    // class MemberImmediateBase<First = {}, Second = {}> {
+    //     member: Second;
+
+    //     setMember(member: Second) {
+    //         return member;
+    //     }
+    // }
+
+    // class MemberImmediate extends MemberImmediateBase {
+    //     member = {
+    //         key: false,
+    //     };
+
+    //     addToState = () => {
+    //         this.setMember({ key: true });
+    //     };
+    // }
+
+    // class MemberCurriedBase<First = {}, Second = {}> {
+    //     member: Second;
+
+    //     setMember(getMember: (oldMember: Second) => Second) {
+    //         getMember(this.member);
+    //     }
+    // }
+
+    // class MemberCurried extends MemberCurriedBase {
+    //     member = {
+    //         key: false,
+    //     };
+
+    //     addToState = () => {
+    //         this.setMember(previousMember => ({
+    //             key: !previousMember.key,
+    //         }));
+    //     };
+    // }
+
+    // Temp: trying to create minimum repro from client TitleEdit...
+
+    class TitleEdit extends Component {
+        state = {
+            hasDeleteRevealed: false,
+        };
+
+        toggleDeleteRevealed = () =>
+            this.setState(prevState => ({
+                hasDeleteRevealed: !prevState.hasDeleteRevealed,
+            }));
     }
 })();
