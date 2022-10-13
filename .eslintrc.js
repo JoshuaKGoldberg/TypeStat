b0721eb4823aa62002fb46d967c9135106b64466
Josh Goldberg <git@joshuakgoldberg.com>
2022-10-13 12:21:05 +0100

    Fixed compile overlaps

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
     extends: ["plugin:@typescript-eslint/recommended", "plugin:@typescript-eslint/recommended-requiring-type-checking", "prettier"],
     parser: "@typescript-eslint/parser",
     parserOptions: {
-        project: "tsconfig.json",
+        project: "tsconfig.eslint.json",
         sourceType: "module",
     },
     plugins: ["deprecation", "no-only-tests"],
