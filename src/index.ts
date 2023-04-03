6b55c792a9ae063cb602f70dc803c6ee5b7a21bb
Emerson <emerson.day@gmail.com>
2023-04-02 18:34:46 -0700

    moved text into one print instead of three

--- src/index.ts
+++ src/index.ts
@@ -53,9 +53,8 @@ export const typeStat = async (argv: TypeStatArgv, output: ProcessOutput): Promi
     }
 
     output.stdout(chalk.greenBright("🚀 Welcome to TypeStat!"));
-    output.stdout(chalk.yellowBright("⚠️ TypeStat is still very early stage and experimental. ⚠️"))
-    output.stdout(chalk.yellowBright("While it will improve your code, it will likely add syntax and type errors."))
-    output.stdout(chalk.yellowBright("Use TypeStat as a starting point before you manually fix and verify any changes."))
+    output.stdout(chalk.yellowBright("⚠️ TypeStat is still very early stage and experimental. ⚠️ While it will improve your code, it will likely add syntax and type errors. Use TypeStat as a starting point before you manually fix and verify any changes."))
+
 
 
     output.stdout(
