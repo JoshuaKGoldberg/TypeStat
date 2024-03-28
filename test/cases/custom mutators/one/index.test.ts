7550823c49cddb7c8894beada40a96655bd412b1
rubiesonthesky <rubiesonthesky>
2024-03-28 23:12:30 +0200

    all the rest test cases

--- test/cases/custom mutators/one/index.test.ts
+++ test/cases/custom mutators/one/index.test.ts
@@ -1,9 +1,9 @@
 import { describe, expect, it } from "vitest";
-import { runMutationForTest } from "../../../src/tests/testSetup.js";
+import { runMutationForTest } from "../../../../src/tests/testSetup.js";
 
-describe("infinite wave detection", () => {
+describe("custom mutators - one", () => {
 	it("matches snapshot", async () => {
 		const resultFile = await runMutationForTest(import.meta.dirname);
 		expect(resultFile).toMatchFileSnapshot("./expected.ts");
 	});
-}, 50000);
+});
---
+++ test/cases/fixes/incompleteTypes/interfaceOrTypeLiteralGenerics/index.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from "vitest";
+import { runMutationForTest } from "../../../../../src/tests/testSetup.js";
+
+describe("incompleteTypes - interfaceOrTypeLiteralGenerics", () => {
+	it("matches snapshot", async () => {
+		const resultFile = await runMutationForTest(import.meta.dirname);
+		expect(resultFile).toMatchFileSnapshot("./expected.ts");
+	});
+});
