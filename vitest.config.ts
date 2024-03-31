5f8d240934d7a7b74e011708320bf224f9ecab89
rubiesonthesky <rubiesonthesky>
2024-03-31 17:20:15 +0300

    include test files into tsconfig, remove test setup file from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
 		clearMocks: true,
 		coverage: {
 			all: true,
-			exclude: ["lib"],
+			exclude: ["lib", "src/tests"],
 			include: ["src"],
 			reporter: ["html", "lcov"],
 		},
