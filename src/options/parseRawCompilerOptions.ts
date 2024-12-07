2ee210111da99000ddcc41c69e56816d68b2b7ea
rubiesonthesky <2591240+rubiesonthesky@users.noreply.github.com>
2024-12-07 18:08:09 +0200

    chore: add common createParseConfigHost function

--- src/options/parseRawCompilerOptions.ts
+++ src/options/parseRawCompilerOptions.ts
@@ -1,10 +1,7 @@
-import { glob } from "glob";
-import { minimatch } from "minimatch";
-import * as fs from "node:fs";
 import * as fsp from "node:fs/promises";
-import * as path from "node:path";
 import ts from "typescript";
 
+import { createParseConfigHost } from "../services/createParseConfigHost.js";
 import { stringifyDiagnosticMessageText } from "../shared/diagnostics.js";
 
 export interface ParsedCompilerOptions extends ts.CompilerOptions {
@@ -28,20 +25,7 @@ export const parseRawCompilerOptions = async (
 	// TSConfig includes often come in a glob form like ["src"]
 	config.include &&= ts.parseJsonConfigFileContent(
 		compilerOptions.config,
-		{
-			fileExists: (filePath) => fs.statSync(filePath).isFile(),
-			readDirectory: (rootDir, extensions, excludes, includes) =>
-				includes
-					.flatMap((include) => glob.sync(path.join(rootDir, include)))
-					.filter(
-						(filePath) =>
-							!excludes?.some((exclude) => minimatch(filePath, exclude)) &&
-							extensions.some((extension) => filePath.endsWith(extension)),
-					)
-					.map((filePath) => path.relative(rootDir, filePath)),
-			readFile: (filePath) => fs.readFileSync(filePath).toString(),
-			useCaseSensitiveFileNames: true,
-		},
+		createParseConfigHost(),
 		cwd,
 	).fileNames;
 
