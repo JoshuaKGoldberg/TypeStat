db1e0b88bfeeac6ca8debaf7f3e06f2cee0c5fbd
rubiesonthesky <2591240+rubiesonthesky@users.noreply.github.com>
2024-12-07 20:00:20 +0200

    fix eslint ignore comments

--- src/services/createParseConfigHost.ts
+++ src/services/createParseConfigHost.ts
@@ -1,18 +1,15 @@
+/* eslint-disable @typescript-eslint/unbound-method */
 import ts from "typescript";
 
 export const createParseConfigHost = (): ts.ParseConfigHost => {
 	return {
-		// eslint-disable-next-line @typescript-eslint/unbound-method
 		directoryExists: ts.sys.directoryExists,
-		// eslint-disable-next-line @typescript-eslint/unbound-method
 		fileExists: ts.sys.fileExists,
 		getCurrentDirectory: () => process.cwd(),
-		// eslint-disable-next-line @typescript-eslint/unbound-method
 		getDirectories: ts.sys.getDirectories,
-		// eslint-disable-next-line @typescript-eslint/unbound-method
 		readDirectory: ts.sys.readDirectory,
-		// eslint-disable-next-line @typescript-eslint/unbound-method
 		readFile: ts.sys.readFile,
 		useCaseSensitiveFileNames: true,
 	};
 };
+/* eslint-enable @typescript-eslint/unbound-method */
